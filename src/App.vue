3b7edba0ca2510d28eaa7132eea12067d86bb6e0
oskla <oskar@oskar.pm>
2023-06-06 03:26:04 +0200

    Mount directly to <body>

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <script setup lang="tsx"></script>
 
-<template>Hello</template>
+<template><div class="app">Hello</div></template>
 
 <style scoped>
-	#app,
-	.vApp {
+	.app {
+		height: 100%;
 		display: flex;
 		flex: 1;
 		flex-direction: column;
 		font-family: Helvetica, Arial, sans-serif;
-		margin: 0;
 		min-height: 100%;
 	}
 </style>
